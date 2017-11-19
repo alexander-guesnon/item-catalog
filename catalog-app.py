6fde743f7c356ce4a239c8bf2a291f76b8ad4d0c
alexgooz <alexgooz92fs@gmail.com>
2017-11-18 22:14:57 -0800

    I created a new form object to best fit this project

--- catalog-app.py
+++ catalog-app.py
@@ -3,3 +3,8 @@ import cgi
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///items.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session =DBSession()
--- database_setup.py
+++ database_setup.py
@@ -7,35 +7,19 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-##classes these are represantation of tables##
-
-
-class Restaurant(Base):
-    __tablename__ = "restaurant"
-
+class Items(Base):
+    __tablename__ = "items"
+    
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-
-
-class MenuItem(Base):
-    __tablename__ = "menu_item"
-
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    course = Column(String(250))
+    category = Column(String(250)) ##may turn this into its own table and then make this a relationship
     description = Column(String(250))
-    price = Column(String(8))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-
-
-####### insert at end of file ########
 
 engine = create_engine(
-    'sqlite:///restaurantmenu.db')
+    'sqlite:///items.db')
 
 Base.metadata.create_all(engine)
 
 
 if __name__ == "__main__":
-    print ("end of program")
+    print ("works great")
