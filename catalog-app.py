ecf753c72ecc600bea21e2a315dce3d678785e68
alexgooz <alexgooz92fs@gmail.com>
2017-11-18 22:28:44 -0800

    setting up base http server

--- catalog-app.py
+++ catalog-app.py
@@ -8,3 +8,37 @@ engine = create_engine('sqlite:///items.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session =DBSession()
+
+
+class WebServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/something"):
+                print("test")
+                
+        except IOError:
+            self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/somethingelse"):
+                print("post a test")
+            
+        except Exception:
+            pass
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)
+        print "Web server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:  
+        print "Stopping web server..."
+        server.socket.close()
+
+
+if __name__ == "__main__":
+    main()
+    print "server terminated"
