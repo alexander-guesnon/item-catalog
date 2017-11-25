fd42a196bb4668e7fd47512c777f70ffc77d6fe6
alexgooz <alexgooz92fs@gmail.com>
2017-11-25 03:17:49 -0800

    implementing categories

--- catalog-app.py
+++ catalog-app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from sqlalchemy import create_engine
+from flask import Flask, render_template, abort
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Items
 
@@ -11,30 +11,31 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+
 @app.route("/")
 def root():  # put var here to pass to document
-    # make pree stored querrys for larg rewquset volumes
+    # make pree stored querrys for larg rewquset volumes]
     ItemOBJ = session.query(Items).all()
-    for i in ItemOBJ:
-        print(i.name)
-        print(i.id)
-        print(i.category)
-        print(i.description)
+    ItemsCatalog = session.query(Items.category).group_by(Items.category).all()
     # commo goes here to pass to document
-    return render_template('root.html', ItemOBJ=ItemOBJ)
+    return render_template('root.html', ItemOBJ=ItemOBJ, ItemsCatalog=ItemsCatalog)
 
 # TODO dynamic url
 
 
-@app.route("/<itemPath>")
-def category(itemPath):
-    print (itemPath)
-    return render_template('category.html')
+@app.route("/<categoryPath>")
+def category(categoryPath):
+    ItemOBJ = session.query(Items).filter(
+        func.lower(Items.category) == func.lower(categoryPath))
+    if 0 == ItemOBJ.count():
+        abort(404)
+    ItemsCatalog = session.query(Items.category).group_by(Items.category).all()
+    return render_template('category.html', ItemOBJ=ItemOBJ, ItemsCatalog=ItemsCatalog)
 
 
 # TODO add custome template per item and  TODO dynamic url
-@app.route("/soccer/stuff")
-def item():
+@app.route("/<categoryPath>/<itemPath>")
+def item(categoryPath, itemPath):
     return "soccer item"
 
 
